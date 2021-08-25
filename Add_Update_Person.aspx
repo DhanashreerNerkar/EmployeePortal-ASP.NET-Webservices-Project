b831478933b4c20ac108443df22f6a827a3dc070
DhanashreeNerkar <57758147+DhanashreerNerkar@users.noreply.github.com>
2021-08-25 11:20:20 +0530

    Add files via upload

---
+++ Add_Update_Person.aspx
@@ -0,0 +1,222 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Add_Update_Person.aspx.cs" Inherits="Add_Update_Person" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+          function ImagePreview(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#<%=Image2.ClientID%>').prop('src', e.target.result)
+                        .width(100)
+                        .height(100);
+                };
+                reader.readAsDataURL(input.files[0]);
+                }
+            }
+
+    </script>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 293px;
+        }
+        .auto-style4 {
+            width: 121px;
+        }
+        .auto-style5 {
+            width: 196px;
+        }
+        .auto-style7 {
+            width: 91px;
+        }
+        .auto-style8 {
+            width: 201px;
+        }
+        .auto-style9 {
+            width: 181px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>   
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="X-Large" Font-Underline="True" Text="Add/Edit Person"></asp:Label>
+        <br />
+        <br />
+        <asp:Panel ID="Panel1" runat="server" Height="184px" Width="1055px" GroupingText="General Information">
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Label ID="lbl_fnm" runat="server" Text="First Name"></asp:Label>
+                    </td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtbox_fnm" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Label ID="lbl_cty" runat="server" Text="City"></asp:Label>
+                    </td>
+                    <td class="auto-style8">
+                        <asp:TextBox ID="txtbox_cty" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style9">
+                        <asp:Label ID="lbl_ise" runat="server" Text="Is Self Employed"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:RadioButtonList ID="RadioButtonList_semp" runat="server" AutoPostBack="True" RepeatDirection="Horizontal" OnSelectedIndexChanged="RadioButtonList_semp_SelectedIndexChanged">
+                            <asp:ListItem Value="Y">Yes</asp:ListItem>
+                            <asp:ListItem Value="N">No</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Label ID="lbl_lnm" runat="server" Text="Last Name"></asp:Label>
+                    </td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtbox_lnm" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Label ID="lbl_state" runat="server" Text="State"></asp:Label>
+                    </td>
+                    <td class="auto-style8">
+                        <asp:TextBox ID="txtbox_state" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style9">
+                        <asp:Label ID="lbl_doj" runat="server" Text="Date Of Joining" TextMode="Date"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtbox_doj" runat="server" TextMode="Date" CausesValidation="True"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Label ID="lbl_design" runat="server" Text="Designation"></asp:Label>
+                    </td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtbox_design" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Label ID="lbl_cntry" runat="server" Text="Country"></asp:Label>
+                    </td>
+                    <td class="auto-style8">
+                        <asp:DropDownList ID="DDL_cntry" runat="server" AutoPostBack="True" BackColor="#CCCCCC" ForeColor="#003366">
+                        </asp:DropDownList>
+                    </td>
+                    <td class="auto-style9">
+                        <asp:Label ID="lbl_dob" runat="server" Text="Date Of Birth"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtbox_dob" runat="server" TextMode="Date"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Label ID="lbl_org" runat="server" Text="Organization"></asp:Label>
+                    </td>
+                    <td class="auto-style5">
+                        <asp:TextBox ID="txtbox_orgazn" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Label ID="lbl_zip" runat="server" Text="Zip"></asp:Label>
+                    </td>
+                    <td class="auto-style8">
+                        <asp:TextBox ID="txtbox_zip" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                    </td>
+                    <td colspan="2">&nbsp;</td>
+                </tr>
+            </table>
+        </asp:Panel>  
+        <br />
+    </div>      
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Panel ID="Panel2" runat="server" GroupingText="Photograph" Height="224px" Width="261px">
+                        <asp:FileUpload ID="FileUpload1" runat="server" Width="251px" onchange="ImagePreview(this);" />
+                        <br />
+                        <br />
+                        <asp:Image ID="Image2" runat="server" Height="90px" ImageUrl="~/images/imgupload.png" Width="80px" />
+                        <br />
+                        <br />
+                        <asp:Button ID="Btn_imguplod" runat="server" Text="Upload" Width="125px" OnClick="Btn_imguplod_Click" />
+                        <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/images/delete.png" Width="20px" />
+                        <br />
+                    </asp:Panel>
+                </td>
+                <td>
+                    <asp:Panel ID="Panel3" runat="server" GroupingText="Contact Details ">
+                        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>       
+    
+                        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                            <ContentTemplate>
+                                <table class="auto-style1">
+                            <tr>
+                                 <td>
+                                    <asp:Label ID="lbl_cnttyp" runat="server" Text="Contact Type"></asp:Label>
+                                    &nbsp;<asp:DropDownList ID="DDL_cnttyp" runat="server" AutoPostBack="True" BackColor="#CCCCCC" ForeColor="#003366">
+                                    </asp:DropDownList>
+                                </td>
+                                <td>
+                                    <asp:Label ID="lbl_cntdetls" runat="server" Text="Contact Details"></asp:Label>
+                                    &nbsp;<asp:TextBox ID="txtbox_cnt" runat="server" BackColor="#CCCCCC"></asp:TextBox>
+                                </td>
+                                <td>
+                                    <asp:Button ID="btn_add" runat="server" OnClick="btn_add_Click" Text="Add" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="3">
+                                    <br />
+                                    <asp:GridView ID="GridView_cntdetails" runat="server" AutoGenerateColumns="False"  OnRowDataBound="GridView_cntdetails_RowDataBound" OnRowDeleting="GridView_cntdetails_RowDeleting" OnRowEditing="GridView_cntdetails_RowEditing" >
+                                        <Columns>
+                                            <asp:BoundField DataField="ContactType" HeaderText="Contact Type" SortExpression="ContactType" />
+                                            <asp:BoundField DataField="Contactdetails" HeaderText="Contact Details" SortExpression="Contactdetails" />
+                                            <asp:TemplateField>
+                                                <ItemTemplate>
+                                                    <asp:LinkButton ID="lnkbtn_delete" runat="server" CommandName="Delete" >Delete</asp:LinkButton>
+                                                </ItemTemplate>
+                                            </asp:TemplateField>
+                                            <asp:TemplateField>
+                                                <ItemTemplate>
+                                                    <asp:LinkButton ID="linkbtn_edit" runat="server" CommandName="Edit" >Edit</asp:LinkButton>
+                                                    </ItemTemplate>
+                                                    <EditItemTemplate>
+                                                        <asp:LinkButton Text="Update" runat="server" OnClick="OnUpdate" />
+                                                        <asp:LinkButton Text="Cancel" runat="server" OnClick="OnCancel" />
+                                                    </EditItemTemplate>                                                
+                                            </asp:TemplateField>
+                                        </Columns>
+                                    </asp:GridView>
+                                </td>
+                               
+                            </tr>
+                        </table>
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </asp:Panel>
+                </td>
+                <td>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <asp:Button ID="btn_save" runat="server" Text="Save" OnClick="btn_save_Click" />&nbsp;
+                    <asp:Button ID="btn_cancel" runat="server" Text="Cancel" />
+                </td>
+            </tr>
+        </table>     
+    </form>
+</body>
+</html>
---
+++ Add_Update_Person.aspx.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using ServiceReference1_ValidateDOJ;
+using ServiceReference2_ValidateDOB;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Xml.Linq;
+using System.Xml;
+
+public partial class Add_Update_Person : System.Web.UI.Page
+{
+    public string strConnectionString = "Data Source=USER-PC; Integrated Security=True; Initial Catalog=SC_14;";
+    private SqlCommand _sqlCommand;
+    private SqlDataReader _sqlDataReader;
+    private SqlDataAdapter _sqlDataAdapter;
+    DataTable dt;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            BindCountryDropDown();
+            BindContactTypeDropDown();
+
+            dt = new DataTable();
+            if (ViewState["Records"] == null)
+            {          
+                dt.Columns.Add("ContactType");
+                dt.Columns.Add("Contactdetails");
+                ViewState["Records"] = dt;
+            }
+        }
+    }
+    public void CreateConnection()
+    {
+        SqlConnection _sqlConnection = new SqlConnection(strConnectionString);
+        _sqlCommand = new SqlCommand();
+        _sqlCommand.Connection = _sqlConnection;
+    }
+    public void OpenConnection()
+    {
+        _sqlCommand.Connection.Open();
+    }
+    public void CloseConnection()
+    {
+        _sqlCommand.Connection.Close();
+    }
+    public void DisposeConnection()
+    {
+        _sqlCommand.Connection.Dispose();
+    }
+    public void BindContactTypeDropDown()
+    {
+        try
+        {
+            CreateConnection();
+            OpenConnection();
+
+            _sqlCommand.CommandText = "Sp_selectcontacttype";
+            _sqlCommand.CommandType = CommandType.StoredProcedure;
+            _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+            dt = new DataTable();
+            _sqlDataAdapter.Fill(dt);
+            _sqlCommand.ExecuteReader();
+
+            //DDL_cnttyp.Items.Insert(0, new ListItem("select", "select"));
+            DDL_cnttyp.DataTextField = "Contacttype";
+            DDL_cnttyp.DataValueField = "ID";
+            DDL_cnttyp.DataSource = dt;
+            DDL_cnttyp.DataBind();
+        }
+        catch (Exception exp)
+        { }
+        finally
+        {
+            CloseConnection();
+            DisposeConnection();
+        }      
+    }
+    public void BindCountryDropDown()
+    {
+        try
+        {
+            CreateConnection();
+            OpenConnection();
+
+            _sqlCommand.CommandText = "Sp_selectcountry";
+            _sqlCommand.CommandType = CommandType.StoredProcedure;
+            _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+            dt = new DataTable();
+            _sqlDataAdapter.Fill(dt);
+            _sqlCommand.ExecuteReader();
+            DDL_cntry.DataTextField = "Name";
+            DDL_cntry.DataValueField = "ID";
+            DDL_cntry.DataSource = dt;
+            DDL_cntry.DataBind();
+        }
+        catch (Exception exp)
+        { }
+        finally
+        {
+            CloseConnection();
+            DisposeConnection();
+        }
+    }
+    static Boolean x;
+    protected void RadioButtonList_semp_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (RadioButtonList_semp.SelectedValue == "Y")
+        {
+            txtbox_doj.Enabled = false;
+            x = true;
+        }
+        else
+        {
+            txtbox_doj.Enabled = true;
+            x = false;
+        }
+    }  
+    protected void btn_save_Click(object sender, EventArgs e)
+    {
+        //WebService_ValidateDOJSoapClient doj = new WebService_ValidateDOJSoapClient();
+        //Int32 ok1 = doj.CheckDateOfJoining(txtbox_doj.Text);
+        //if (ok1 == 0)
+        //{
+        //    txtbox_doj.Text = "";
+        //    txtbox_doj.ForeColor = System.Drawing.Color.Red;
+        //    txtbox_doj.Focus();
+        //}
+
+        //WebService_ValidateDOBSoapClient dob = new WebService_ValidateDOBSoapClient();
+        //Int32 ok2 = dob.CheckDateOfBirth(txtbox_dob.Text, txtbox_doj.Text);
+        //if (ok2 == 0)
+        //{
+        //    txtbox_dob.Text = "";
+        //    txtbox_dob.ForeColor = System.Drawing.Color.Red;
+        //    txtbox_dob.Focus();
+        //}
+
+        //**xml file**
+        dt = (DataTable)ViewState["Records"];
+        string xmfilename = "PersonContacts.xml";
+        dt.WriteXml("C:/" + xmfilename);
+        var xmlcontacts = File.ReadAllText("C:/PersonContacts.xml");
+        try
+        {
+            CreateConnection();
+            OpenConnection();
+
+            _sqlCommand.CommandText = "Sp_InsertPerson";
+            _sqlCommand.CommandType = CommandType.StoredProcedure;
+            
+
+            _sqlCommand.Parameters.AddWithValue("@param_fnm", txtbox_fnm.Text);                                     //First Name 
+            _sqlCommand.Parameters.AddWithValue("@param_lnm", txtbox_lnm.Text);                                      //Last Name
+            _sqlCommand.Parameters.AddWithValue("@param_dob", Convert.ToDateTime(txtbox_dob.Text));                       //DOB
+            _sqlCommand.Parameters.AddWithValue("@param_organization", txtbox_orgazn.Text);                               //Organization
+            _sqlCommand.Parameters.AddWithValue("@param_designation", txtbox_design.Text);                                //Designation 
+            //_sqlCommand.Parameters.AddWithValue("@param_country", Convert.ToInt32(DDL_cntry.SelectedIndex));
+            //_sqlCommand.Parameters.Add("@param_country", SqlDbType.Int).Value = DDL_cntry.SelectedIndex;//Country
+            _sqlCommand.Parameters.AddWithValue("@param_state", txtbox_state.Text);                                       //State
+            _sqlCommand.Parameters.AddWithValue("@param_city", txtbox_cty.Text);                                          //City
+            _sqlCommand.Parameters.AddWithValue("@param_zip", txtbox_zip.Text);                                       //Zip Code            
+            _sqlCommand.Parameters.AddWithValue("@param_photoname", img);  //image file
+            _sqlCommand.Parameters.AddWithValue("@param_photocontent", bytes);                                           //image
+            //_sqlCommand.Parameters.AddWithValue("@param_isselfemp", x);//is self emp ?
+            _sqlCommand.Parameters.AddWithValue("@param_doj", Convert.ToDateTime(txtbox_doj.Text));                       //DOJ
+            _sqlCommand.Parameters.AddWithValue("@param_contacts", xmlcontacts);                                           //contact details
+
+            _sqlCommand.ExecuteNonQuery();
+
+        }
+        catch (Exception exp)
+        { }
+        finally
+        {
+            CloseConnection();
+            DisposeConnection();
+        }
+
+        
+
+        //string constr = strConnectionString;
+        //using (SqlConnection conn = new SqlConnection(constr))
+        //{
+        //    string sql = "INSERT INTO tblxml VALUES(@xmldata)";
+        //    using (SqlCommand cmd = new SqlCommand(sql, conn))
+        //    {
+        //        cmd.Parameters.AddWithValue("@xmldata", xmlcontacts);
+        //        conn.Open();
+        //        cmd.ExecuteNonQuery();
+        //        conn.Close();
+        //    }
+        //}
+        //dt = (DataTable)ViewState["Records"];
+        //int metaIndex = 0;
+        //string xmlcontacts = ToXml(dt, metaIndex);
+        //XmlDocument xDoc = new XmlDocument();
+        //xDoc.LoadXml(xmlcontacts);
+        
+
+
+    }
+    //public string ToXml(DataTable table, int metaIndex)
+    //{
+    //    XDocument xdoc = new XDocument(
+    //        new XElement(table.TableName,
+    //            from column in table.Columns.Cast<DataColumn>()
+    //            where column != table.Columns[metaIndex]
+    //            select new XElement(column.ColumnName,
+    //                from row in table.AsEnumerable()
+    //                select new XElement(row.Field<string>(metaIndex), row[column])
+    //                )
+    //            )
+    //        );
+    //    return xdoc.ToString();
+    //}
+    protected void btn_add_Click(object sender, EventArgs e)
+    {
+        dt = (DataTable)ViewState["Records"];
+        dt.Rows.Add(DDL_cnttyp.SelectedItem.ToString(), txtbox_cnt.Text);
+        GridView_cntdetails.DataSource = dt;
+        GridView_cntdetails.DataBind();
+    }
+    protected void GridView_cntdetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int index = Convert.ToInt32(e.RowIndex);
+        dt = (DataTable)ViewState["Records"];
+        dt.Rows[index].Delete();
+        ViewState["Records"] = dt;
+        GridView_cntdetails.DataSource = dt;
+        GridView_cntdetails.DataBind();
+    }
+    protected void GridView_cntdetails_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            string item = e.Row.Cells[0].Text;
+            foreach (LinkButton lnkbtn_del in e.Row.Cells[2].Controls.OfType<LinkButton>())
+            {
+                if (lnkbtn_del.CommandName == "Delete")
+                {
+                    lnkbtn_del.Attributes["onclick"] = "if(!confirm('Do you want to delete " + item + "?')){ return false; };";
+                }
+                if (lnkbtn_del.CommandName == "Edit")
+                {
+                    txtbox_cnt.Text = GridView_cntdetails.Rows[GridView_cntdetails.SelectedIndex].Cells[1].Text;
+                }
+            }
+        }        
+    }
+    protected void GridView_cntdetails_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView_cntdetails.EditIndex = e.NewEditIndex;
+        dt = (DataTable)ViewState["Records"];
+        ViewState["Records"] = dt;
+        GridView_cntdetails.DataSource = dt;
+        GridView_cntdetails.DataBind();
+    }
+    protected void OnUpdate(object sender, EventArgs e)
+    {
+        GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+        string cntyp = (row.Cells[0].Controls[0] as TextBox).Text;
+        string cntdel = (row.Cells[1].Controls[0] as TextBox).Text;
+        DataTable dt = (DataTable)ViewState["Records"];
+        dt.Rows[row.RowIndex]["ContactType"] = cntyp;
+        dt.Rows[row.RowIndex]["Contactdetails"] = cntdel;
+        ViewState["Records"] = dt;
+        GridView_cntdetails.EditIndex = -1;
+        GridView_cntdetails.DataSource = dt;
+        GridView_cntdetails.DataBind();
+    }
+    protected void OnCancel(object sender, EventArgs e)
+    {
+        GridView_cntdetails.EditIndex = -1;
+        GridView_cntdetails.DataSource = dt;
+        GridView_cntdetails.DataBind();
+    }
+   
+    public static string img = "", imgfilename="";
+    static byte[] bytes;
+    protected void Btn_imguplod_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            string fname = FileUpload1.FileName;
+            FileUpload1.SaveAs(Server.MapPath("~/UploadedImages/" + fname));
+            img = "~/UploadedImages/" + fname;
+        }
+
+        using (BinaryReader br = new BinaryReader(FileUpload1.PostedFile.InputStream))
+        { bytes = br.ReadBytes(FileUpload1.PostedFile.ContentLength); }
+        imgfilename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+    }
+}
